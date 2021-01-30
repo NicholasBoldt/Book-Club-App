2c640dd10d7b97f90663f764300a8fa8738d9122
unknown <sunruolun@gmail.com>
2021-01-30 12:47:19 -0500

    myclubs function

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+from .models import Club
 
 # Create your views here.
 def home(request):
     print("Working")
+
+def clubs_index(request):
+  clubs = Club.objects.all()
+  return render(request, 'clubs/index.html', { 'clubs': clubs })
