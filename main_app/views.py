178aea1c70217d0a7bc4e8174be118dfde811491
Mark Shyzer <markshyzer@Micky-McBoo.local>
2021-01-31 01:26:37 -0500

    search improvements

--- main_app/views.py
+++ main_app/views.py
@@ -3,24 +3,19 @@ import requests
 import os 
 from .models import Book, Rec
 
-ISBN_ROOT = 'https://openlibrary.org/api/books'
-
 # Create your views here.
 def home(request):
     print("Working")
 
-
 def select_book(request):
     books = None
-    if request.GET:
+    if request.GET: # isbn search
         if 'isbn' in request.GET:
             isbn = [request.GET['isbn']]
             books = search_isbn(isbn)
-        # print('Body: ', request.GET['isbn'])
-        elif 'search_title' in request.GET:
+        elif 'search_title' in request.GET: # author/title search
             books = search_title_author(request.GET['search_title'], request.GET['search_author'])
-
-    elif request.POST:
+    elif request.POST: # add selected title to database
         print(request.POST)
         print(request.POST['title'])
         new_book = Book(
@@ -31,35 +26,16 @@ def select_book(request):
             image=request.POST['image_link'],
             )
         # new_book.save()
-
         books = None
-
     return render(request, 'selectbook.html', { 'books' : books})
 
-def search_isbn(isbn_list): # takes a list of ISBN numbers and returns a list of objects containing book, author, isbn, desc and image_link 
+def search_isbn(isbn_list): # takes a list of ISBN numbers and returns a list of objects containing book, author, isbn, desc and image
     books = []
     my_key = os.environ['GOOGLE_BOOKS_API_KEY']
     for i, isbn in enumerate(isbn_list):
-        print('ISBN is: ', isbn)
-        # r = requests.get('https://openlibrary.org/api/books?bibkeys=ISBN:' + str(isbn) +'&jscmd=data&format=json').json()
-        # isbn_key = 'ISBN:' + str(isbn)
-        # if isbn_key in r:
-        #     title = r[isbn_key]['title']
-        #     author = r[isbn_key]['authors'][0]['name']
-        #     print(title)
-        #     if 'cover' in r[isbn_key]:
-        #         image_link = r[isbn_key]['cover']['medium']
-        #     else:
-        #         image_link = None
-        #     books.append({
-        #         'title': title,
-        #         'author': author,
-        #         'image_link': image_link,
-        #         'isbn' : isbn,
-        #     })
         r = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:' + str(isbn) + '&printType=BOOKS&key=' + my_key).json()
         if r['totalItems'] == 0:
-            books.append({ 'error' : 'ISBN not found. Please check your search and try again'})
+            books.append({ 'error' : 'Book not found. Please check your search and try again'})
         else:
             title = r['items'][0]['volumeInfo']['title']
             author = r['items'][0]['volumeInfo']['authors'][0]
@@ -76,37 +52,25 @@ def search_isbn(isbn_list): # takes a list of ISBN numbers and returns a list of
                 'isbn' : isbn,
                 'desc' : desc,
             })
-
-        # else:
-        #     books.append({ 'error' : 'ISBN not found. Please check your search and try again'})
-
-
-
     return books
 
-def search_title_author(search_title, search_author):
+def search_title_author(search_title, search_author): # searches title and author keywords to return a list of isbns, then uses search_isbn to return a list of objects containing book, author, isbn, desc, and image
     MAX = 5 #maximum titles to return
-    isbn = []
+    isbn = [0]
     my_key = os.environ['GOOGLE_BOOKS_API_KEY']
     r = requests.get('https://www.googleapis.com/books/v1/volumes?q=' + search_title + '+inauthor:' + search_author +'&maxResults=' + str(MAX) + '&orderBy=relevance&printType=BOOKS&key=' + my_key).json()
-    # r = requests.get('https://www.googleapis.com/books/v1/volumes?q=Wizard+inauthor:Frank&maxResults=5&orderBy=relevance&printType=BOOKS&key=AIzaSyBCwPi28d-g2G8eaYfWVPYsbibwOaerf88').json()
-    # print('Searching...')
-
-    for x in range(5):
+    if r['totalItems'] < MAX:
+        MAX = r['totalItems']
+    for x in range(MAX):
         if 'industryIdentifiers' in r['items'][x]['volumeInfo']:
             num = r['items'][x]['volumeInfo']['industryIdentifiers'][0]['identifier']
             if num.isnumeric(): # filters out non-ISBN identifiers, which contain letter codes
                 isbn.append(num)
 
-
-
     books = search_isbn(isbn)
 
     return books
 
-
-
-
-
+# Some test ISBNs:
 # 9781609618957
 # 9780140441185
